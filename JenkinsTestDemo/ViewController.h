591b6ba8df76c34e60c99cce8f7c8c24c0c1528b
chenBW <861754186@qq.com>
2016-12-20 23:40:58 +0800

    init

---
+++ JenkinsTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsTestDemo
+//
+//  Created by 陈博文 on 16/12/20.
+//  Copyright © 2016年 陈博文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,4 @@
+platform:ios, '8.0'
+target ‘JenkinsTestDemo’ do
+pod 'MJExtension'
+end
