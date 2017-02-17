08eb26ea43cfe1bcd9aba9e44bf3b4418b9507bd
chenBW <861754186@qq.com>
2017-02-17 15:04:26 +0800

    增加 MWBrowser

--- Pods/Target Support Files/Pods-JenkinsTestDemo/Pods-JenkinsTestDemo-resources.sh
+++ Pods/Target Support Files/Pods-JenkinsTestDemo/Pods-JenkinsTestDemo-resources.sh
@@ -76,6 +76,15 @@ EOM
       ;;
   esac
 }
+if [[ "$CONFIGURATION" == "Debug-Test" ]]; then
+  install_resource "$PODS_CONFIGURATION_BUILD_DIR/MWPhotoBrowser/MWPhotoBrowser.bundle"
+fi
+if [[ "$CONFIGURATION" == "Release-AdHoc" ]]; then
+  install_resource "$PODS_CONFIGURATION_BUILD_DIR/MWPhotoBrowser/MWPhotoBrowser.bundle"
+fi
+if [[ "$CONFIGURATION" == "Release-AppStore" ]]; then
+  install_resource "$PODS_CONFIGURATION_BUILD_DIR/MWPhotoBrowser/MWPhotoBrowser.bundle"
+fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
